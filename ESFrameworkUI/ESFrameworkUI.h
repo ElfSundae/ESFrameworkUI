cebd37d57e3aef5dc4896d9031522edceea97b52
Elf Sundae <elf.sundae@gmail.com>
2019-08-21 10:33:13 +0800

    imports

--- ESFrameworkUI/ESFrameworkUI.h
+++ ESFrameworkUI/ESFrameworkUI.h
@@ -3,7 +3,7 @@
 //  ESFramework
 //
 //  Created by Elf Sundae on 2019/04/15.
-//  Copyright © 2019 https://0x123.com. All rights reserved.
+//  Copyright © 2014-2019 https://0x123.com. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -16,8 +16,6 @@ FOUNDATION_EXPORT const unsigned char ESFrameworkUIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ESFrameworkUI/PublicHeader.h>
 
-#if __has_include(<ESFrameworkUI/ESActivityLabel.h>)
-
 #import <ESFrameworkUI/ESActivityLabel.h>
 #import <ESFrameworkUI/ESBadgeView.h>
 #import <ESFrameworkUI/ESErrorView.h>
@@ -25,15 +23,3 @@ FOUNDATION_EXPORT const unsigned char ESFrameworkUIVersionString[];
 #import <ESFrameworkUI/ESRefreshControlDefaultContentView.h>
 #import <ESFrameworkUI/ESStatusOverlayView.h>
 #import <ESFrameworkUI/ESTableViewController.h>
-
-#else
-
-#import "ESActivityLabel.h"
-#import "ESBadgeView.h"
-#import "ESErrorView.h"
-#import "ESRefreshControl.h"
-#import "ESRefreshControlDefaultContentView.h"
-#import "ESStatusOverlayView.h"
-#import "ESTableViewController.h"
-
-#endif
